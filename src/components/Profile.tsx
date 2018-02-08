f86758c26b557c78b763e0ab24145c6a68c969f5
akserg <akserg@gmail.com>
2018-02-08 11:43:00 +0200

    Added  redux store

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,9 +5,11 @@ import './Profile.css';
 export interface Props {
   name: string;
   enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
 }
 
-function Profile({ name, enthusiasmLevel = 1 }: Props) {
+function Profile({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
   if (enthusiasmLevel <= 0) {
     throw new Error('You could be a little more enthusiastic. :D');
   }
@@ -16,6 +18,10 @@ function Profile({ name, enthusiasmLevel = 1 }: Props) {
     <div className="hello">
       <div className="greeting">
         Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
       </div>
     </div>
   );
